91655f578415e6df29096f91ee49bb75ffe006a9
Charlie Sale <softwaresale01@gmail.com>
2019-03-23 22:51:35 -0400

    absbackend: backend now inherits from ABC
    Wasn't an abstract base class before. Now, both are good.

--- crossclip/absbackend.py
+++ crossclip/absbackend.py
@@ -21,7 +21,7 @@ import sys
 import os
 from abc import ABC, abstractmethod, abstractstaticmethod, abstractproperty
 
-class AbstractBackend(object):
+class AbstractBackend(ABC):
     """ Interface for all clipboard backends
 
     This class is an interface for all clipboard backends to
@@ -29,34 +29,64 @@ class AbstractBackend(object):
     """
     @abstractmethod
     def get_text(self):
+        """ Synchronously gets text from clipboard
+        :returns: Text from clipboard
+        :rtype: str
+        """
         pass
 
     @abstractmethod
-    def get_image(self, format='Pil'):
+    def get_image(self, form):
+        """ Synchronously gets image from clipboard
+        :returns: Image from clipboard
+        :rtype:
+        """
         pass
 
     @abstractmethod
     def set_text(self, text):
+        """ Sets text to the clipboard
+        :param text: Text to add
+        :type text: str
+        """
         pass
 
     @abstractmethod
-    def set_image(self, img, format='pil'):
+    def set_image(self, img, form):
+        """ Sets image to clipboard
+        :param img: Image to set to clipboard
+        :type img:
+        :param form: Format of image
+        :type form: str
+        """
         pass
 
-class AbstractImageConverter:
-    
+class AbstractImageConverter(ABC):
+    """ Converts an image between a Pillow Image and a native clipboard image
+
+    This interface is needed to convert native images to pillow images and vice
+    versa.
+    """
     @abstractproperty
     def image_type(self):
+        """ The type of native image
+        """
         pass
 
     @abstractproperty
     def image_str(self):
+        """ String representing image
+        """
         pass
 
     @abstractmethod
-    def to_pillow(native_image):
+    def to_pillow(self, native_image):
+        """ Converts a native image to Pillow iamge
+        """
         pass
 
     @abstractmethod
-    def from_pillow(pil):
+    def from_pillow(self, pil):
+        """ Converts a pillow image to native type
+        """
         pass
